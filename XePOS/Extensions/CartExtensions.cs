1d6ca0ae1c1374687ae663ff8e42aabca5fdfd79
Dustin Tran <dustin.tran@proptechgroup.io>
2022-02-12 22:14:43 +1300

    Add extensions and self-review

---
+++ XePOS/Extensions/CartExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace XePOS.Application.Extensions;
+
+public static class CartExtensions
+{
+    public static void ScanProductRange(this PointOfSaleTerminal pos,string range)
+    {
+        Console.WriteLine($"Scanned products: {range}");
+        _ = range.Select(c => pos.ScanProduct(c.ToString())).ToArray();
+    }
+}
